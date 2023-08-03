89432f9d10e528cc129e884cf92431575bd9ed5c
horserio <111210772+horserio@users.noreply.github.com>
2023-08-03 10:53:17 +0900

    JS-project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // 💡Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,39 @@
 'use strict'
+
+//프로젝트 필러링 관련 로직 처리
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+
+categories.addEventListener('click', (event) => { 
+    const filter = event.target.dataset.category;
+    if(filter == null) { 
+        return;
+    }
+    handleActiveSelection(event.target);
+    filterProjects (filter);
+});
+
+
+function handleActiveSelection(target){
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.classList.add('category--selected');
+}
+
+function filterProjects (filter) {
+    projects.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.category) {
+        project.style.display = 'block';
+        } else {
+        project.style.display = 'none';
+        }
+    });
+
+    projectsContainer.classList.add('anim-out')
+    setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+    }, 250);
+    }
